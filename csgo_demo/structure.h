a8e0c4d53f7fed9db68b06e601844d3857ed86f9
zhengyuhang <zhengyuhang@x-cmd.com>
2023-11-11 02:18:33 +0800

    update

---
+++ csgo_demo/structure.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<Windows.h>
+using namespace std;
+
+struct vec4{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+struct vec3 {
+	float x;
+	float y;
+	float z;
+};
+struct vec2 {
+	float x;
+	float y;
+};
+
