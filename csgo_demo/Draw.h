a300c77f0f97d217c531f82048b36e141fcc140f
zhengyuhang <zhengyuhang@x-cmd.com>
2023-11-11 02:17:15 +0800

    update

--- csgo_demo/Draw.h
+++ csgo_demo/Draw.h
@@ -1,10 +1,20 @@
 #pragma once
 #include<Windows.h>
+#include"structure.h"
+#include"Offsets.h"
 using namespace std; 
 class Draw
 {
 public:
 	RECT rectGame; 
 	HWND hExwnd;
+	BOOL WorldToScreen(vec3& worldpos, vec2& screenpos);		
+	RECT rect;
+	RECT rectex;
+	int width;
+	int height;
+	int widthex;
+	int heightex;
+	void GetWindowInfo();
 };
 extern Draw draw; 
