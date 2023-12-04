e82dd6a825b42e564bcbfa2b647304b5b3ce7fb0
zhengyuhang <zhengyuhang@x-cmd.com>
2023-12-05 01:10:22 +0800

    update

--- csgo_demo/aimbot.cpp
+++ csgo_demo/aimbot.cpp
@@ -60,41 +60,39 @@ void CalAngle(vec3 FOV, vec3 headpos, vec2& CalcLocalAngle, vec2& calLocalAngleD
 	local_angle.y = mem.readmemory<float>(offsets.clientbase + offsets.m_fViewAngle + 0x0);
 	local_angle.x = mem.readmemory<float>(offsets.clientbase + offsets.m_fViewAngle + 0x4);
 
-	if (headpos.x >= FOV.x && headpos.y >= FOV.y) //第一象限
+	if (headpos.x >= FOV.x && headpos.y >= FOV.y) {
+		CalcLocalAngle.x = (float)((double)atan2(headpos.y - FOV.y, headpos.x - FOV.x)* 180/3.1415);
+
+	}else if (headpos.x <= FOV.x && headpos.y >= FOV.y) 
 	{
-		CalcLocalAngle.x = (float)((double)atan2(headpos.y - FOV.y, headpos.x - FOV.x) * 180 / 3.1415); //弧度
+		CalcLocalAngle.x = 180 - (float)((double)atan2(headpos.y - FOV.y, FOV.x - headpos.x ) * 180 / 3.1415);
 	}
-	if (headpos.x <= FOV.x && headpos.y >= FOV.y) //第二象限
+	else if (headpos.x <= FOV.x && headpos.y <= FOV.y)
 	{
-		CalcLocalAngle.x = 180 - (float)((double)atan2(headpos.y - FOV.y, FOV.x - headpos.x) * 180 / 3.1415); //弧度
+		CalcLocalAngle.x = - 180 + (float)((double)atan2(FOV.y - headpos.y , FOV.x - headpos.x  ) * 180 / 3.1415);
 	}
-	if (headpos.x <= FOV.x && headpos.y <= FOV.y) //第三象限
+	else if (headpos.x >= FOV.x && headpos.y <= FOV.y)
 	{
-		CalcLocalAngle.x = -180 + (float)((double)atan2(FOV.y - headpos.y, FOV.x - headpos.x) * 180 / 3.1415); //弧度
+		CalcLocalAngle.x = -(float)((double)atan2(FOV.y - headpos.y, FOV.x - headpos.x) * 180 / 3.1415);
 	}
-	if (headpos.x >= FOV.x && headpos.y <= FOV.y) //第四象限
-	{
-		CalcLocalAngle.x = -(float)((double)atan2(FOV.y - headpos.y, headpos.x - FOV.x) * 180 / 3.1415); //弧度
+	float distance = sqrt((headpos.x - FOV.x) * (headpos.x - FOV.x) + (headpos.y - FOV.y) * (headpos.y - FOV.y));
+	if (headpos.z >= FOV.z) {
+		CalcLocalAngle.y = -(float)((double)atan2(headpos.z - FOV.z , distance) * 180 / 3.1415);
 	}
-	float 平面直线距离 = sqrt((headpos.x - FOV.x) * (headpos.x - FOV.x) + (headpos.y - FOV.y) * (headpos.y - FOV.y));
-	//高低朝向
-	if (headpos.z >= FOV.z) //上方
-	{
-		CalcLocalAngle.y = -(float)((double)atan2(headpos.z - FOV.z, 平面直线距离) * 180 / 3.1415); //弧度
+	if (headpos.z <= FOV.z) {
+		CalcLocalAngle.y = (float)((double)atan2(FOV.z - headpos.z , distance) * 180 / 3.1415);
 	}
-	if (headpos.z <= FOV.z) //下方
-	{
-		CalcLocalAngle.y = (float)((double)atan2(FOV.z - headpos.z, 平面直线距离) * 180 / 3.1415); //弧度
-	}
-	//计算水平朝向角度差 
+
 	calLocalAngleDiff.x = local_angle.x - CalcLocalAngle.x;
-	if (calLocalAngleDiff.x > 180) {
-		calLocalAngleDiff.x -= 360;
+
+	if(calLocalAngleDiff.x > 180) {
+		calLocalAngleDiff.x = 360 - calLocalAngleDiff.x;
 	}
-	if (calLocalAngleDiff.x < -180) {
-		calLocalAngleDiff.x += 360;
+
+	if (calLocalAngleDiff.x < - 180) {
+		calLocalAngleDiff.x = 360 + calLocalAngleDiff.x;
 	}
-	//高低朝向角度差
-	calLocalAngleDiff.y = CalcLocalAngle.y - local_angle.y;
+
+	calLocalAngleDiff.y = CalcLocalAngle.y - local_angle.y ;
 
 }
