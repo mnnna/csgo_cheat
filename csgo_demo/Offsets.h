d00a4785a3d07c55fbcf89fb8e2dcbd7e326c9af
zhengyuhang <zhengyuhang@x-cmd.com>
2023-12-06 01:08:47 +0800

    update

--- csgo_demo/Offsets.h
+++ csgo_demo/Offsets.h
@@ -5,14 +5,14 @@ using namespace std;
 class Offsets
 {
 public :
-	DWORD64 dwEntityList = 0x16CF6D0;
-	DWORD64 deLocalPlayer = 0x16C29E8;
-	DWORD64 deViewMatrix = 0x1819C10;
+	DWORD64 dwEntityList = 0x16CF800;
+	DWORD64 deLocalPlayer = 0x16C2B18;
+	DWORD64 deViewMatrix = 0x1819D40;
 
 	DWORD m_iTeamNum = 0xD48;
 	DWORD mHealth = 0xA6C;
 	DWORD m_fPOS = 0xCD8;
-	DWORD m_fViewAngle = 0x187A8C0;
+	DWORD m_fViewAngle = 0x187A9D0;
 
 
 	HWND hwnd = 0;
